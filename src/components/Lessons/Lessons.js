8f77f5f5d580c8a453b4f5ae7b04b42386fb6da3
sospinar21 <sospinar21@gmail.com>
2018-05-05 18:56:25 -0600

    Add iframe

--- src/components/Lessons/Lessons.js
+++ src/components/Lessons/Lessons.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react'
 import './Lessons.css'
 import AddLessons from '../AddLessons/AddLessons'
+import Iframe from 'react-iframe'
 
 class Lessons extends Component {
   constructor() {
     super()
     this.state = {
-
+      urlInput: ''
     }
+  }
+
+  showInput = (input) => {
+    this.setState({
+      urlInput: input
+    })
+    // return this.url
   }
 
 
 render() {
+  console.log(this.url)
   return (
     <div className='lessons'>
-      <AddLessons />
+      <AddLessons showInput={this.showInput}/>
+      <iframe 
+        className='lessonIframe'
+        src= {this.state.urlInput}
+      ></iframe>
     </div>
   )
 }
